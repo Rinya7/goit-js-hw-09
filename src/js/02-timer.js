b2c24cb1952e50f2b2a653acada0dbeb62a98ff8
Rinya7 <v.dmitriev0712@gmail.com>
2023-05-11 15:57:12 +0200

    2 css_alert

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,19 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   input: document.querySelector('#datetime-picker'),
   days: document.querySelectorAll('.value'),
   btnStart: document.querySelector('button[data-start]'),
+  timerCss: document.querySelector('.timer'),
+  fieldSection: document.querySelectorAll('.field'),
 };
 
 refs.btnStart.addEventListener('click', callbackStartTimer);
 refs.btnStart.setAttribute('disabled', 'disabled');
-
 const DELAY = 1000;
+cssTimer();
 
 const options = {
   enableTime: true,
@@ -21,9 +24,15 @@ const options = {
   onClose(selectedDates) {
     const time = selectedDates[0] - new Date();
     if (time < 0) {
-      alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future', {
+        cssAnimationStyle: 'zoom',
+      });
+
       return;
     }
+    Notify.success('Date selected. Press strat to "START" the timer', {
+      cssAnimationStyle: 'zoom',
+    });
     refs.btnStart.removeAttribute('disabled');
     const dataInput = convertMs(time);
     setDataToStart(dataInput);
@@ -74,4 +83,23 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+
+function cssTimer() {
+  const key = Object.values(refs.days);
+  for (let i = 0; i < key.length; i++) {
+    refs.days[i].style.display = 'block';
+    refs.days[i].style.textAlign = 'center';
+  }
+
+  refs.timerCss.style.display = 'flex';
+  refs.timerCss.style.marginTop = '20px';
+
+  const keys = Object.values(refs.fieldSection);
+  console.log(keys);
+  for (let i = 0; i < keys.length; i++) {
+    refs.fieldSection[i].style.marginRight = '20px';
+    refs.fieldSection[i].style.padding = '5px';
+    refs.fieldSection[i].style.outline = 'auto';
+  }
 }
